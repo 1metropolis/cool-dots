87dbfeaa932286e89af189c9677be0a5839adece
Metro <48939070+mtropls@users.noreply.github.com>
2024-02-12 12:27:50 -0800

    Added ZSH, Kitty, and NVIM dots

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,14 @@
+# Visual 'top' command
+alias top='zfxtop'
+
+# Changing "ls" to "exa"
+alias ls='exa --icons --color=always --group-directories-first'
+alias ll='exa -alhF --icons --color=always --group-directories-first'
+alias la='exa -a --icons --color=always --group-directories-first'
+alias l='exa -F --icons --color=always --group-directories-first'
+
+# Change su to su -l
+alias su="su -l"
+
+# global 'eww' command
+alias eww="$HOME/.eww/target/release/eww"
